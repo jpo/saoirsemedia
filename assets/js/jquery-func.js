0ad9c5b13bca8c8d31170ce1030607064ae46a55
Josh O'Rourke <joshorourke@me.com>
2015-04-12 18:43:21 -0400

    Initial commit

---
+++ assets/js/jquery-func.js
@@ -0,0 +1,14 @@
+!function ($) {
+    $body.scrollspy({
+        target: '#navbar-main',
+        offset: navHeight
+    });
+
+    $window.on('load', function () {
+        $body.scrollspy('refresh');
+    });
+
+    $('#navbar-main [href=#]').click(function (e) {
+        e.preventDefault();
+    });
+};
